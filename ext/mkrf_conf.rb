a2b8b9038a2637566c4a5c0ec486d65e423cb32c
machzqcq <pradeep@seleniumframework.com>
2015-04-20 18:04:00 -0400

    supporting the gem for non-jruby just in case user installs

---
+++ ext/mkrf_conf.rb
@@ -0,0 +1,19 @@
+# Borrowed from http://www.programmersparadox.com/2012/05/21/gemspec-loading-dependent-gems-based-on-the-users-system/
+# The author mentions that this is not the right way to do when installing on different rubies like mruby, jruby, but
+# seems there is no other solution at this time, if a single gem has to work on both mruby and jruby
+
+begin
+  if RUBY_VERSION > "1.7" and RUBY_PLATFORM == "java"
+    installer.install "jruby-win32ole", ">=0"
+  else
+    installer.install "win32screenshot", ">=0"
+  end
+
+rescue
+  #Exit with a non-zero value to let rubygems know something went wrong
+  exit(1)
+end
+
+f = File.open(File.join(File.dirname(__FILE__), "Rakefile"), "w")
+f.write("task :default\n")
+f.close
--- te3270.gemspec
+++ te3270.gemspec
@@ -18,6 +18,7 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
+  spec.extensions = ["ext/mkrf_conf.rb"]
 
   spec.add_dependency 'page_navigation', '>= 0.9'
   spec.add_dependency 'jruby-win32ole'
